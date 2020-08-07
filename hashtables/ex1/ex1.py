dda0eec4be64c76bb935e985a690c50b4934d75e
Kenneth T. Barrett <kennethtbarrett@criptext.com>
2020-08-07 10:12:47 -0500

    ex1 complete (tests pass)

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -2,6 +2,19 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    cache = {}
 
-    return None
+    # Iterate through range(length)
+    # by our index, adding existing weights
+    # by index.
+    for i in range(length):
+        cache[weights[i]] = i
+
+    # Find weights already in cache.
+    for index, weight in enumerate(weights):
+        target_weight = limit - weight  # Subtract weight from limit.
+        # Check if that value is in cache. If it is...
+        if target_weight in cache:
+            i = cache[target_weight]  # Set i to its value
+            # And return with the appropriate formatting.
+            return (i, index) if index < i else (index, i)
