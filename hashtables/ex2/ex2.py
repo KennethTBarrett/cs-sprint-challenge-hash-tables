02ba87bb0ba0fd76f46febaaeba9c8f6e2df1ff9
Kenneth T. Barrett <kennethtbarrett@criptext.com>
2020-08-07 11:09:16 -0500

    All assignments complete (this was the hardest one for me)

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -1,4 +1,3 @@
-#  Hint:  You may not need all of these.  Remove the unused functions.
 class Ticket:
     def __init__(self, source, destination):
         self.source = source
@@ -9,6 +8,20 @@ def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    # Setup
+    route = [None] * length
+    cache = {}
 
-    return route
+    # For all of the tickets we have...
+    for ticket in tickets:
+        # Set the ticket source in our cache to be our destination
+        cache[ticket.source] = ticket.destination
+    
+    # Because our final destination must be "NONE"
+    next = cache['NONE']
+
+    # Iterate through, adding stop.
+    for current in range(length):
+        route[current] = next
+        next = cache[next]
+    return route  # Return our route
