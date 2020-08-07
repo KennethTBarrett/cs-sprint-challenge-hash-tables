1f943dd6a0a28758e9328cee6af0a0529df83a26
Kenneth T. Barrett <kennethtbarrett@criptext.com>
2020-08-07 10:50:37 -0500

    ex5 complete (tests pass)

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -1,13 +1,28 @@
-# Your code here
-
-
-
 def finder(files, queries):
     """
     YOUR CODE HERE
     """
-    # Your code here
-
+    cache = {}
+    # For every item in the files, we need to
+    # make our split, and append the item if
+    # in cache.
+    for path in files:
+        # Get final part, set as item, check
+        # if in cache.
+        item = path.split('/')[-1]
+        if item in cache:
+            cache[item].append(path)
+        else:
+            cache[item] = [path]
+    # For every query we have, check if it's in the cache.
+    # If the query exists in our cache, we're going to append
+    # the path of the results of that query of our cache.
+    result = []
+    for query in queries:
+        if query in cache:
+            results = cache[query]
+            for path in results:
+                result.append(path)
     return result
 
 
